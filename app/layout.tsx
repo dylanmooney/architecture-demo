392b38e23d6ae5538bbd6df9596cb2afe736e752
Dylan Mooney <105092927+dylanmooney@users.noreply.github.com>
2023-10-03 13:42:19 +0100

    fix(layout): this is a fix for that feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        New Feature
+        New Feature!
         {children}
       </body>
     </html>
